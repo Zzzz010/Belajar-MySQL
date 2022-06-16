84f5f2de6c504df3680bc5a0e7cd0764e3fe621b
Zzzz010 <75777303+Zzzz010@users.noreply.github.com>
2022-06-16 13:38:10 +0700

    Update Pengenalan - Table.sql

--- Pengenalan - Table/Pengenalan - Table.sql
+++ Pengenalan - Table/Pengenalan - Table.sql
@@ -1,5 +1,12 @@
 desc barang;
 
+alter table jual_beli_online
+add column nama varchar(30), // menambah colom baru
+drop column nama_hapus, // menghapus colom
+rename column kode to kode_barang, // mengubah nama colom
+modify id_pelanggan TEXT first,
+modify harga_barang INT after nama_barang;
+
 alter table barang
 modify kode_barang int not null,
 modify id_pembeli int not null;
